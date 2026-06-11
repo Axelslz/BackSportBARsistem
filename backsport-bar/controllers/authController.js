8ebe70745abc5a7bfbc738d37e690b708974d083
Axelslz <axlsalazarr1602@gmail.com>
2026-06-11 13:38:22 -0600

    updates

--- backsport-bar/controllers/authController.js
+++ backsport-bar/controllers/authController.js
@@ -45,7 +45,7 @@ export const login = async (req, res) => {
 
     const token = jwt.sign(
       { id: user.id, role: user.role }, 
-      process.env.JWT_SECRET,
+      process.env.JWT_SECRET || 'CazadoresSportBarSecretKey2026'
       { expiresIn: '12h' }
     );
 
